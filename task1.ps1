9a7a1cc9e8f23e7b2c942a40182701232a3762e3
pavel.voronin <pavel.voronin@i.ua>
2017-01-22 11:06:19 +0200

    fixs

--- task1.ps1
+++ task1.ps1
@@ -25,44 +25,52 @@ Name: NewLocalPassword
 Default Value: !TestLocalAdmin 
 Description: Password of new local admin
 -----------------------
+Type: String Parameter
+Name: Computer
+Default Value: 
+Description: List of computers
+-----------------------
 #>
 # List of computers
-$computers = @("Win10Comp1","Win10Comp2","Win10Comp3","Win10Comp4","Win10Comp5","Win10Comp6","Win10Comp7") 
-#$computers = Import-CSV C:\Computers.csv | select Computer #or we can read from CSV file
+#$computers = @("Comp1","IE9WIN7")
 $Result_log= @()
 #Convert password to SecureString format
 $Pwd = ConvertTo-SecureString -AsPlainText $env:AD_password -Force
-$CurrentCred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $env:AD_admin, $Pwd
-
+$CurrentCred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $env:AD_Admin_User, $Pwd
+echo $env:AD_Admin_User
 #Connect to AD_Server and execute script on ScriptBlock
-$Jenkins_log = Invoke-Command -ComputerName $env:AD_server -Credential $CurrentCred -ScriptBlock { param($computers,$env:NewLocalAdmin,$env:NewLocalPassword,$Result_log)
-Foreach ($computer in $computers) {
-    $users = $null
-    $comp = [ADSI]"WinNT://$computer"
-     #Check if username exists   
-        $users = ($computer.psbase.children | Where-Object {$_.psBase.schemaClassName -eq "User"} | Select-Object -expand Name)
-        if ($users -contains $env:NewLocalAdmin) {
- 			$Result_log += Write-Output "$computer already contain Local admin account"
+
+Foreach ($computer in $env:Computers) {
+   $users = $null
+   $comp = [ADSI]"WinNT://$computer"
+           $users = ($comp.psbase.children | Where-Object {$_.psBase.schemaClassName -eq "User"} | Select-Object -expand Name)
+       
+#Check if username exists  
+       if ($users -contains $env:NewLocalAdmin) {
+          $Result_log += Write-Output "Computer: $computer already contain Local admin account named: $env:NewLocalAdmin"
         } else {
-            #Create the account
-            $user = $comp.Create("User","$env:NewLocalAdmin")
-            $user.SetPassword("env:NewLocalPassword")
-            $user.Put("Description","New Local Admin from Task1")
-            $user.Put("Fullname","TestName")
-            $user.SetInfo()         
-            #Add the account to the local admins group
-            $group = [ADSI]"WinNT://$computer/Administrators,group"
-            $group.add("WinNT://$computer/$env:NewLocalAdmin")
- 
+             #Create the account
+             $user = $comp.Create(“user”, “$env:NewLocalAdmin”)
+             $user.Put(“description”, “$env:NewLocalAdmin”)
+             $user.SetPassword($env:NewLocalPassword)
+             $user.SetInfo()
+             $user.psbase.invokeset(“AccountDisabled”, “False”)
+             $user.SetInfo()
+             #Add the account to the local admins group
+             $group = [ADSI]"WinNT://$computer/Administrators,group"
+             $group.add("WinNT://$computer/$env:NewLocalAdmin")
+        }
+       
                 #Validate what user account has been created or not
-                $users = ($computer.psbase.children | Where-Object {$_.psBase.schemaClassName -eq "User"} | Select-Object -expand Name)
-                if ($users -contains $env:NewLocalAdmin) {
-					$Result_log += Write-Output "Account $env:NewLocalAdmin created on $computer"
+                $users = ($comp.psbase.children | Where-Object {$_.psBase.schemaClassName -eq "User"} | Select-Object -expand Name)
+                if ($users -contains $NewLocalAdmin) {
+			    	$Result_log += Write-Output "Account $env:NewLocalAdmin created on computer: $computer"
                 } else {
-				    $Result_log += Write-Output "Account $env:NewLocalAdmin not created on $computer"
+				    $Result_log += Write-Output "Account $env:NewLocalAdmin not created on computer: $computer"
                 }
-          }
-		}
+         }
+		
 return $Result_log
+$Result_log > C:\temp\log.txt
 } -ArgumentList $computers,$NewLocalAdmin,$NewLocalPassword, $Result_log
 $Jenkins_log > C:\log.txt
